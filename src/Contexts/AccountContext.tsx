8f86baf75499540beeac10b7158e0c82f557adb9
Franky <funfrank4591@gmail.com>
2024-11-20 10:12:49 +0530

    Account context added

---
+++ src/Contexts/AccountContext.tsx
@@ -0,0 +1,25 @@
+import React, { createContext, ReactNode, useState } from "react";
+
+
+  interface AccountContextProviderProp {
+    children: ReactNode;
+  }
+  
+  
+export const AccountContext = createContext<{
+    accountAddress: string | null;
+  setAccount: React.Dispatch<React.SetStateAction<string | null>>;
+} >({
+  accountAddress: null,
+  setAccount: () => {},
+});
+
+export const AccountProvider = ({ children  } : AccountContextProviderProp) => {
+  const [account, setAccount] = useState<string | null>(null);
+
+  return (
+    <AccountContext.Provider value={{ accountAddress: account, setAccount }}>
+      {children}
+    </AccountContext.Provider>
+  );
+};
